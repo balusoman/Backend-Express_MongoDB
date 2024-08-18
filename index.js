6212d5e81f00c171f6093e60658229986282545b
balusoman <balucritaxzz@gmail.com>
2024-08-18 20:35:56 +0530

    some

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use.urlencoded({ extended: false });
 
 // routes
 app.use("/api/products", productRoute);
@@ -15,12 +15,14 @@ app.get("/", (req, res) => {
   res.send("Hello from Node API Server Updated");
 });
 
-
-mongoose.connect('mongodb+srv://balu:ZrpnlHo77a3Cxaf6@backenddb.8d5xwtt.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB')
+mongoose
+  .connect(
+    "mongodb+srv://balu:ZrpnlHo77a3Cxaf6@backenddb.8d5xwtt.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB"
+  )
   .then(() => {
     console.log("Connected to database!");
     app.listen(3000, () => {
-      console.log("Server is running on port 30000");
+      console.log("Server is running on port 3000");
     });
   })
   .catch(() => {
